509ec240370689e1d8507d06a0fb2b22d1262f7e
crazy-refrigerator <weicl17@connect.hku.hk>
2024-10-02 20:57:29 +0800

    Update crayonshinchan.py
    remove some explanation

--- crayonshinchan.py
+++ crayonshinchan.py
@@ -1,7 +1,6 @@
 from turtle import *
 
 def draw_crayonshinchan():
-    # 设置画笔速度
     speed(6)
 
     penup()
@@ -62,44 +61,41 @@ def draw_crayonshinchan():
     circle(10)
     end_fill()
 
-    penup()  # 抬起画笔
+    penup()  
     pensize(10)
-    goto(-60, 0)  # 移动到起点
+    goto(-60, 0) 
     color("black")
     left(90)
-    pendown()  # 落下画笔
+    pendown()  
     length = 20
-    forward(length)  # 绘制长度为 length 的线段
+    forward(length) 
     left(90)
     length = 20
-    forward(length)  # 绘制长度为 length 的线段
+    forward(length) 
 
-    penup()  # 抬起画笔
+    penup() 
     pensize(10)
-    goto(5, -5)  # 移动到起点
+    goto(5, -5)  
     color("black")
     right(90)
-    pendown()  # 落下画笔
+    pendown()  
     length = 20
-    forward(length)  # 绘制长度为 length 的线段
+    forward(length)  
     left(90)
     length = 20
-    forward(length)  # 绘制长度为 length 的线段
+    forward(length)
 
     #ear
     penup()
-    goto(50, -60)  # 移动到右下方位置
-    setheading(0)  # 朝右侧
+    goto(50, -60)  
+    setheading(0) 
     pendown()
-    color("black")  # 选择半圆的颜色
-    circle(15, 180)  # 半径为 100，绘制 180 度的弧
+    color("black") 
+    circle(15, 180) 
 
-    # 完成
     hideturtle()
 
 
-# 调用绘制函数
 draw_crayonshinchan()
 
-# 点击窗口时退出
 exitonclick()
